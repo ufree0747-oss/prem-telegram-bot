50800321cb25696c2d4c3b115b54f6bff649ddcd
ufree0747-oss <ufree0747@gmail.com>
2026-01-13 09:13:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,13 +2,19 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+MY_ID =  6227990433  # replace with your real ID
+
 TOKEN = os.getenv("TOKEN")
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def start(update, context):
+    if update.effective_user.id != MY_ID:
+        return
     await update.message.reply_text("Hey Prem 👋 Your personal bot is active 24/7!")
+
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
 app.run_polling()
+
 
