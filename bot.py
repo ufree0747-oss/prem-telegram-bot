977d1a848dfdabf9f96d77da0f2336527fcb6151
ufree0747-oss <ufree0747@gmail.com>
2026-01-13 09:20:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,5 @@
 from datetime import date
 import json
-
-DATA_FILE = "data.json"
-
-def load_data():
-    try:
-        with open(DATA_FILE, "r") as f:
-            return json.load(f)
-    except:
-        return {}
-
-def save_data(data):
-    with open(DATA_FILE, "w") as f:
-        json.dump(data, f)
-
 import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
@@ -26,7 +12,18 @@ async def start(update, context):
     if update.effective_user.id != MY_ID:
         return
     await update.message.reply_text("Hey Prem 👋 Your personal bot is active 24/7!")
+DATA_FILE = "data.json"
 
+def load_data():
+    try:
+        with open(DATA_FILE, "r") as f:
+            return json.load(f)
+    except:
+        return {}
+
+def save_data(data):
+    with open(DATA_FILE, "w") as f:
+        json.dump(data, f)
 async def log(update, context):
     if update.effective_user.id != MY_ID:
         return
@@ -60,6 +57,7 @@ app.add_handler(CommandHandler("start", start))
 app.add_handler(CommandHandler("log", log))
 app.add_handler(CommandHandler("today", today))
 app.run_polling()
+
 
 
 
